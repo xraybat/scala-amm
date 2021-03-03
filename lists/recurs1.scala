085e7d1e4046bca2e7d93d4bf4034fdf5519bec3
xraybat <xraybat100@gmail.com>
2021-03-03 14:50:17 +1100

    new recurs1.scala

---
+++ lists/recurs1.scala
@@ -0,0 +1,37 @@
+
+def removeFours(lst: List[Int], acc: List[Int] = List.empty): List[Int] = lst match {
+  case Nil    => acc.reverse
+  case 4 :: t => removeFours(t, acc)
+  case h :: t => removeFours(t, h :: acc)
+}
+
+def traverse(lst: List[Int], acc: List[Int] = List.empty): List[Int] = lst match {
+  case Nil    => acc.reverse
+  case h :: t => traverse(t, h :: acc)
+}
+
+def reverse(lst: List[Int], acc: List[Int] = List.empty): List[Int] = lst match {
+  case Nil    => acc
+  case h :: t => reverse(t, h :: acc)
+}
+
+def sum(lst: List[Int]): Int = lst match {
+  case Nil => 0
+  case h :: t => h + sum(t)
+}
+
+//////////////////////////////////////////////////////////////////////
+import ammonite.ops._
+
+// (ಠ_ಠ)
+@main
+def main(args: String*) = {
+  val lst = 1 :: 2 :: 3 :: 4 :: 5 :: Nil  // 'Nil' reqd
+  println(lst)
+
+  println("removeFours: " + removeFours(lst))
+  println("traverse: " + traverse(lst))
+  println("reverse: " + reverse(lst))     // same as `lst.reverse`
+  println("sum: " + sum(lst))
+
+}
